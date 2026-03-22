b672e2b42e1b5ecb6c026c82968e900f4de1123d
Geralld Borbon <eternalmangocean@gmail.com>
2026-03-22 02:03:52 -0700

    fix(flake): fix flake.nix buildShell warning

--- flake.nix
+++ flake.nix
@@ -5,28 +5,28 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in
+  outputs = { self, nixpkgs } @inputs:
+  let
+    supportedSystems = [ "x86_64-linux" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    # Function to build a dev shell for a given system
+    buildShell = { system }:
+    let pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell {
+      buildInputs = with pkgs; [
+        python314
+        uv
+        ty
+        ruff
+        black
+      ];
+    };
+  in
     {
       # Define dev shells for all supported systems
       devShells = forAllSystems (system: {
-        default = self.buildShell { inherit system; };
+        default = buildShell { inherit system; };
       });
-
-      # Function to build a dev shell for a given system
-      buildShell = { system }:
-        let pkgs = import nixpkgs { inherit system; };
-        in pkgs.mkShell {
-          buildInputs = with pkgs; [
-            python314
-            uv
-            ty
-            ruff
-            black
-          ];
-        };
     };
 }
