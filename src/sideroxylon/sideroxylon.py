951d5254e0d5f091b15f7eed7695d26ab08ddf93
Geralld Borbon <eternalmangocean@gmail.com>
2026-04-01 15:13:50 -0700

    refactor(sideroxylon): modularize sideroxylon function

--- src/sideroxylon/sideroxylon.py
+++ src/sideroxylon/sideroxylon.py
@@ -7,7 +7,7 @@ from typing import Annotated
 
 def assign_token_to_headers(token_file):
     """
-    Get headers for GitHub
+    Get headers for GitHub.
     """
 
     # Get contents of token file and store them on GITHUB_TOKEN
@@ -21,14 +21,14 @@ def assign_token_to_headers(token_file):
 
     return repository_headers
 
-def convert_github_url_to_api_url(repo_url):
+def convert_github_url_to_api_url(repository_url):
     """
-    Convert GitHub URL to GitHub API URL
+    Convert GitHub URL to GitHub API URL.
     """
 
-    # Check if we are at the correct position of the url, store user and
+    # Check if we are at the correct position of the URL, store user and
     # repository names, and return the converted URL
-    parts = repo_url.strip().split('/')
+    parts = repository_url.strip().split('/')
 
     if len(parts) < 5:
         return None
@@ -38,20 +38,29 @@ def convert_github_url_to_api_url(repo_url):
 
     return f'https://api.github.com/repos/{user}/{repo}'
 
+def get_urls_inside_repository_url_file(repository_url_file):
+    """
+    Read URLs inside repository_url_file.
+    """
+
+    with open(repository_url_file, 'r') as file:
+        urls: list[str] = [line.strip() for line in file if line.strip()]
+
+    return urls
 
 def get_github_repository_programming_language(repository_url, repository_headers):
     """
-    Get the main programming language of the provided repository url.
+    Get the main programming language of the provided repository URL.
     """
 
-    # Convert normal url to api url
+    # Convert normal URL to api URL
     api_url = convert_github_url_to_api_url(repository_url)
 
-    # Check if api url exists, and if not return Unknown
+    # Check if api URL exists, and if not return Unknown
     if not api_url:
         return 'Unknown'
 
-    # Try to use the token. If the token fails send the url to Unknown.
+    # Try to use the token. If the token fails send the URL to Unknown.
     try:
         response = requests.get(api_url, headers=repository_headers)
         if response.status_code != 200:
@@ -67,27 +76,11 @@ def get_github_repository_programming_language(repository_url, repository_header
         print(f'Error fetching {repository_url}: {e}')
         return 'Unknown'
 
-def sideroxylon(
-    # File that cotains the token
-    token_file: Annotated[str, typer.Option(help='Path to the GitHub token file.')] = 'gh_token.org',
-    # File that contains the repository urls
-    repository_url_file: Annotated[str, typer.Option(help='Path to the repository urls file.')] = 'github_repos.org',
-    # Directory with all the programming language files
-    languages_directory: Annotated[str, typer.Option(help='Path to the directory where urls are stored.')] = 'languages/',
-):
+def store_repository_url_in_corresponding_file(repository_urls, repository_headers, languages_directory):
     """
-    Entry point of the sideroxylon cli.
+    Store each repository URL in the file with the name of its main programming language.
     """
-
-    repository_headers: dict = assign_token_to_headers(token_file)
-
-    # Get each link in the repository url file
-    with open(repository_url_file, 'r') as file:
-        urls: list[str] = [line.strip() for line in file if line.strip()]
-
-    # Once we get the main programming language, put the link in a file with the
-    # same name as the language
-    for url in urls:
+    for url in repository_urls:
         language = get_github_repository_programming_language(url, repository_headers)
 
         filename = (
@@ -107,9 +100,36 @@ def sideroxylon(
         # This line is here to avoid hitting rate limits
         time.sleep(2)
 
-    # Clear the repository url file after going through each link
-    # At some point I will change this so at the beginning of the program it clears all files
+def clean_repository_url_file(repository_url_file):
+    """
+    Clean the file with the repository URLs.
+    """
     open(repository_url_file, 'w').close()
+
+def sideroxylon(
+    # File that cotains the token
+    token_file: Annotated[str, typer.Option(help='Path to the GitHub token file.')] = 'gh_token.org',
+    # File that contains the repository urls
+    repository_url_file: Annotated[str, typer.Option(help='Path to the repository URLs file.')] = 'github_repos.org',
+    # Directory with all the programming language files
+    languages_directory: Annotated[str, typer.Option(help='Path to the directory where URLs are stored.')] = 'languages/',
+):
+    """
+    Entry point of the sideroxylon cli.
+    """
+
+    repository_headers: dict = assign_token_to_headers(token_file)
+
+    # Get each link in the repository URL file
+    urls = get_urls_inside_repository_url_file(repository_url_file)
+
+    # Once we get the main programming language, put the link in a file with the
+    # same name as the language
+    store_repository_url_in_corresponding_file(urls, repository_headers, languages_directory)
+
+    # Clear the repository URL file after going through each link
+    # At some point I will change this so at the beginning of the program it clears all files
+    clean_repository_url_file(repository_url_file)
 
 
 if __name__ == '__main__':
