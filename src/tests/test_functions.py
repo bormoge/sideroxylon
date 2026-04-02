afd771a82b808b9546a8a52a00a4aaa58e6d04da
Geralld Borbon <eternalmangocean@gmail.com>
2026-04-02 02:06:57 -0700

    refactor(tests): add test_dir, github_headers, test_repository fixtures

--- src/tests/test_functions.py
+++ src/tests/test_functions.py
@@ -1,45 +1,53 @@
 from sideroxylon import sideroxylon
 from typing import Any
 import os
+import pytest
 
-def test_assign_token_to_headers():
-    with open('gh_token.org', 'r') as file:
+@pytest.fixture
+def test_dir():
+    return 'src/tests'
+
+@pytest.fixture
+def github_headers(test_dir):
+    return sideroxylon.assign_token_to_headers(f'{test_dir}/gh_token.org')
+
+@pytest.fixture
+def test_repository():
+    return 'https://github.com/bormoge/sideroxylon'
+
+def test_assign_token_to_headers(test_dir, github_headers):
+    with open(f'{test_dir}/gh_token.org', 'r') as file:
         github_token: str = file.read().replace('\n', '')
 
     repository_headers: dict[str, Any] = {}
     repository_headers['Authorization'] = f'token {github_token}'
 
-    assert sideroxylon.assign_token_to_headers('src/tests/gh_token.org') == repository_headers
+    assert github_headers == repository_headers
 
-def test_convert_github_url_to_api_url():
-    test_repository: str = 'https://github.com/bormoge/sideroxylon'
+def test_convert_github_url_to_api_url(test_repository):
     assert sideroxylon.convert_github_url_to_api_url(test_repository) == 'https://api.github.com/repos/bormoge/sideroxylon'
 
-def test_get_urls_inside_repository_url_file():
+def test_get_urls_inside_repository_url_file(test_dir):
     url_list: list[str] = ['https://github.com/bormoge/sideroxylon', 'https://github.com/bormoge/spinosum', 'https://github.com/bormoge/guava-themes']
 
-    assert sideroxylon.get_urls_inside_repository_url_file('src/tests/test_url_file.org') == url_list
+    assert sideroxylon.get_urls_inside_repository_url_file(f'{test_dir}/test_url_file.org') == url_list
 
-def test_get_github_repository_programming_language():
-    headers: dict[str, Any] = sideroxylon.assign_token_to_headers('src/tests/gh_token.org')
-    url_sucess: str = 'https://github.com/bormoge/sideroxylon'
-    url_failure: str = 'https://github.com/bormoge/failure'
+def test_get_github_repository_programming_language(github_headers, test_repository):
+    test_repository_failure: str = 'https://github.com/bormoge/failure'
 
-    assert sideroxylon.get_github_repository_programming_language(url_sucess, headers) == 'Python'
-    assert sideroxylon.get_github_repository_programming_language(url_failure, headers) == 'Unknown'
+    assert sideroxylon.get_github_repository_programming_language(test_repository, github_headers) == 'Python'
+    assert sideroxylon.get_github_repository_programming_language(test_repository_failure, github_headers) == 'Unknown'
 
-def test_store_repository_url_in_corresponding_file():
-    if os.path.isfile('src/tests/Python.txt'):
-        os.remove('src/tests/Python.txt')
+def test_store_repository_url_in_corresponding_file(test_dir, github_headers):
+    if os.path.isfile(f'{test_dir}/Python.txt'):
+        os.remove(f'{test_dir}/Python.txt')
 
-    headers: dict[str, Any] = sideroxylon.assign_token_to_headers('src/tests/gh_token.org')
     url_file: list[str] = ['https://github.com/bormoge/sideroxylon']
-    dir: str = 'src/tests'
 
-    sideroxylon.store_repository_url_in_corresponding_file(url_file, headers, dir, 'txt', 2)
+    sideroxylon.store_repository_url_in_corresponding_file(url_file, github_headers, test_dir, 'txt', 2)
 
-    with open('src/tests/Python.txt', 'r') as file:
+    with open(f'{test_dir}/Python.txt', 'r') as file:
         python_org_contents: str = file.read().replace('\n', '')
 
-    assert os.path.isfile('src/tests/Python.txt')
+    assert os.path.isfile(f'{test_dir}/Python.txt')
     assert python_org_contents == 'https://github.com/bormoge/sideroxylon'
