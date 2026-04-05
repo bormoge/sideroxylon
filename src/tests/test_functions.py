7f9142d40f3aa9f43e05da0fc81992d9c3e8038c
Geralld Borbon <eternalmangocean@gmail.com>
2026-04-05 13:38:05 -0700

    refactor(test): add dump directory

--- src/tests/test_functions.py
+++ src/tests/test_functions.py
@@ -8,36 +8,40 @@ def test_dir():
     return 'src/tests'
 
 @pytest.fixture
-def github_headers(test_dir):
-    return sideroxylon.assign_token_to_headers(f'{test_dir}/gh_token.org')
+def throwaway_dir(test_dir):
+    return f'{test_dir}/throwaway'
+
+@pytest.fixture
+def github_headers(throwaway_dir):
+    return sideroxylon.assign_token_to_headers(f'{throwaway_dir}/gh_token.org')
 
 @pytest.fixture
 def test_repository():
     return 'https://github.com/bormoge/sideroxylon'
 
-def test_initialize_directories_and_files(test_dir):
-    languages_dir = f'{test_dir}/languages_straw/'
-    token_file = f'{test_dir}/straw.org'
+def test_initialize_directories_and_files(throwaway_dir):
+    example_dir = f'{throwaway_dir}/languages_example/'
+    example_file = f'{throwaway_dir}/example.org'
 
     directories_and_files: dict[str, list[str]] = {
-        "directories": [languages_dir],
-        "files": [token_file]
+        "directories": [example_dir],
+        "files": [example_file]
     }
 
-    if os.path.isdir(languages_dir):
-        os.rmdir(languages_dir)
+    if os.path.isdir(example_dir):
+        os.rmdir(example_dir)
 
-    if os.path.isfile(token_file):
-        os.remove(token_file)
+    if os.path.isfile(example_file):
+        os.remove(example_file)
 
     sideroxylon.initialize_directories_and_files(directories_and_files)
 
-    assert os.path.isdir(languages_dir)
-    assert os.path.isfile(token_file)
+    assert os.path.isdir(example_dir)
+    assert os.path.isfile(example_file)
 
 
-def test_assign_token_to_headers(test_dir, github_headers):
-    with open(f'{test_dir}/gh_token.org', 'r') as file:
+def test_assign_token_to_headers(throwaway_dir, github_headers):
+    with open(f'{throwaway_dir}/gh_token.org', 'r') as file:
         github_token: str = file.read().replace('\n', '')
 
     repository_headers: dict[str, Any] = {}
@@ -48,10 +52,17 @@ def test_assign_token_to_headers(test_dir, github_headers):
 def test_convert_github_url_to_api_url(test_repository):
     assert sideroxylon.convert_github_url_to_api_url(test_repository) == 'https://api.github.com/repos/bormoge/sideroxylon'
 
-def test_get_urls_inside_repository_url_file(test_dir):
+def test_get_urls_inside_repository_url_file(throwaway_dir):
     url_list: list[str] = ['https://github.com/bormoge/sideroxylon', 'https://github.com/bormoge/spinosum', 'https://github.com/bormoge/guava-themes']
 
-    assert sideroxylon.get_urls_inside_repository_url_file(f'{test_dir}/test_url_file.org') == url_list
+    test_url_file = f'{throwaway_dir}/test_url_file.org'
+
+    if not os.path.exists(test_url_file) or os.path.getsize(test_url_file) == 0:
+        with open(test_url_file, "w") as file:
+            for url in url_list:
+                file.write(url + "\n")
+
+    assert sideroxylon.get_urls_inside_repository_url_file(f'{throwaway_dir}/test_url_file.org') == url_list
 
 def test_get_github_repository_programming_language(github_headers, test_repository):
     test_repository_failure: str = 'https://github.com/bormoge/failure'
@@ -59,16 +70,16 @@ def test_get_github_repository_programming_language(github_headers, test_reposit
     assert sideroxylon.get_github_repository_programming_language(test_repository, github_headers) == 'Python'
     assert sideroxylon.get_github_repository_programming_language(test_repository_failure, github_headers) == 'Unknown'
 
-def test_store_repository_url_in_corresponding_file(test_dir, github_headers):
-    if os.path.isfile(f'{test_dir}/Python.txt'):
-        os.remove(f'{test_dir}/Python.txt')
+def test_store_repository_url_in_corresponding_file(throwaway_dir, github_headers, test_repository):
+    if os.path.isfile(f'{throwaway_dir}/Python.txt'):
+        os.remove(f'{throwaway_dir}/Python.txt')
 
-    url_file: list[str] = ['https://github.com/bormoge/sideroxylon']
+    test_url: list[str] = [test_repository]
 
-    sideroxylon.store_repository_url_in_corresponding_file(url_file, github_headers, test_dir, 'txt', 2)
+    sideroxylon.store_repository_url_in_corresponding_file(test_url, github_headers, throwaway_dir, 'txt', 2)
 
-    with open(f'{test_dir}/Python.txt', 'r') as file:
+    with open(f'{throwaway_dir}/Python.txt', 'r') as file:
         python_org_contents: str = file.read().replace('\n', '')
 
-    assert os.path.isfile(f'{test_dir}/Python.txt')
+    assert os.path.isfile(f'{throwaway_dir}/Python.txt')
     assert python_org_contents == 'https://github.com/bormoge/sideroxylon'
