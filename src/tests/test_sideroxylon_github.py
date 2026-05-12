5a5c6cffd5663db79f84dcea123ff9a5c00031e5
Geralld Borbon <eternalmangocean@gmail.com>
2026-05-12 11:14:09 -0700

    feat(tests): replace requests with urllib

--- src/tests/test_sideroxylon_github.py
+++ src/tests/test_sideroxylon_github.py
@@ -66,27 +66,27 @@ def test_api_url_no_language(github_forge_object, test_repository_no_language):
 
 
 @pytest.fixture
-def fetched_data_success(github_forge_object, test_api_url_success):
-    fetched_data_success: dict[str, Any] | None = (
-        github_forge_object.fetch_forge_repository_data(test_api_url_success).json()
+def response_success(github_forge_object, test_api_url_success):
+    response_success: dict[str, Any] | None = (
+        github_forge_object.fetch_forge_repository_data(test_api_url_success)
     )
-    return fetched_data_success
+    return response_success
 
 
 @pytest.fixture
-def fetched_data_failure(github_forge_object, test_api_url_failure):
-    fetched_data_failure: dict[str, Any] | None = (
+def response_failure(github_forge_object, test_api_url_failure):
+    response_failure: dict[str, Any] | None = (
         github_forge_object.fetch_forge_repository_data(test_api_url_failure)
     )
-    return fetched_data_failure
+    return response_failure
 
 
 @pytest.fixture
-def fetched_data_no_language(github_forge_object, test_api_url_no_language):
-    fetched_data_no_language: dict[str, Any] | None = (
-        github_forge_object.fetch_forge_repository_data(test_api_url_no_language).json()
+def response_no_language(github_forge_object, test_api_url_no_language):
+    response_no_language: dict[str, Any] | None = (
+        github_forge_object.fetch_forge_repository_data(test_api_url_no_language)
     )
-    return fetched_data_no_language
+    return response_no_language
 
 
 def test_convert_forge_url_to_api_url(test_repository_success, github_forge_object):
@@ -102,14 +102,20 @@ def test_fetch_forge_repository_data(
     test_api_url_failure,
     test_api_url_no_language,
 ):
-    data_success: dict[str, Any] | None = (
-        github_forge_object.fetch_forge_repository_data(test_api_url_success).json()
+    data_success: dict[str, Any] | None = github_forge_object.convert_response_to_dict(
+        github_forge_object.fetch_forge_repository_data(test_api_url_success)
     )
 
     assert max(data_success, key=lambda k: data_success[k]) == "Python"
-    assert github_forge_object.fetch_forge_repository_data(test_api_url_failure).status_code == 404
     assert (
-        github_forge_object.fetch_forge_repository_data(test_api_url_no_language).json() == {}
+        github_forge_object.fetch_forge_repository_data(test_api_url_failure).getcode()
+        == 404
+    )
+    assert (
+        github_forge_object.convert_response_to_dict(
+            github_forge_object.fetch_forge_repository_data(test_api_url_no_language)
+        )
+        == {}
     )
 
 
@@ -121,25 +127,25 @@ def test_get_repository_programming_language(
     test_api_url_success,
     test_api_url_failure,
     test_api_url_no_language,
-    fetched_data_success,
-    fetched_data_failure,
-    fetched_data_no_language,
+    response_success,
+    response_failure,
+    response_no_language,
 ):
     assert (
         github_forge_object.get_repository_programming_language(
-            test_repository_success, fetched_data_success
+            test_repository_success, response_success
         )
         == "Python"
     )
     assert (
         github_forge_object.get_repository_programming_language(
-            test_repository_failure, fetched_data_failure
+            test_repository_failure, response_failure
         )
         == "GitHub_URL"
     )
     assert (
         github_forge_object.get_repository_programming_language(
-            test_repository_no_language, fetched_data_no_language
+            test_repository_no_language, response_no_language
         )
         == "No_Programming_Language"
     )
